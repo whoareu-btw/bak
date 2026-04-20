e8f820837f62fa2f867a0e1ca572a620207498f8
Neko-chi <orangganteng87659@gmail.com>
2026-04-20 14:07:42 +0700

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import sys
+
+print("This script will install package below\n -curl\n -podman\n -nix (multiuser)")
+
+while True:
+    choice = input("This script will use sudo access, do you wanto to give access?\n [y/n]").strip().lower()
+
+    if choice in ["y", "yes"]:
+        print("Let's do this!")
+        break
+    elif choice in ["n", "no"]:
+        print("See ya")
+        break
+    else:
+        print("I'm out")
+        break
+
+print("One more time, this script will use sudo access")
+
+if os.getuid() !=0:
+    print("Get ready!")
+    os.execvp("sudo", ["sudo", sys.executable] + sys.argv)
+    sys.exit(1)
+
+ITEMS = ["curl", "podman"]
+
+subprocess.run(["apt", "install", *ITEMS])
+
+print("Will curling nix from source")
+
+COMMAND = "sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon"
+subprocess.run(["bash", "-c", COMMAND])
+
+print("It's done, soldier!")
